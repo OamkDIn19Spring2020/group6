939d6cd5e75e59d9dde763416586db45fb3987ef
Anthony <t9haan01@students.oamk.fi>
2020-04-23 15:48:32 +0300

    Adding products & purchase model

---
+++ application/models/Purchase_model.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Purchase_model extends CI_Model
+{
+
+    // ------------------------------------------------------------------------
+
+    // Refs: CRUD_model
+    protected $_table = 'purchase_database';
+    protected $_primary_key = 'product_id, user_id';
+
+    // ------------------------------------------------------------------------
+
+}
+
+/* End of file Purchase_model.php */
+/* Location: ./application/models/Purchase_model.php */
